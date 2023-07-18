600513327e160c5306c89cd2e57877304a6e8804
BlackCheetah98 <niranjancheetah98@gmail.com>
2023-07-19 02:09:16 +0530

    const. change

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,6 +2,6 @@
 // export const redirectUrlPrefix = "http://"
 // export const redirectUrlSuffix = ".localhost:8000/"
 
-export const webApiEndPoint = "https://mille.onrender.com//api/v1"
+export const webApiEndPoint = "https://mille.onrender.com/api/v1"
 export const redirectUrlPrefix = "https://"
 export const redirectUrlSuffix = ".mille.onrender.com/"
